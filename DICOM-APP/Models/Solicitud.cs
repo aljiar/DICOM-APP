ddcc4d725a4be789706ff310a7fa26bef7689969
Alan Jimenez Arandia <aljiar_23@hotmail.com>
2019-03-23 19:35:14 -0400

    Adding model solicitud

---
+++ DICOM-APP/Models/Solicitud.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DICOM_APP.Models
+{
+    public class Solicitud
+    {
+        public long Id { get; set; }
+        public string Data { get; set; }
+    }
+}
